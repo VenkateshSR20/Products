ad1610be3d573484f7ba4acbf782562e76765bab
unknown <venkateshsr20@gmail.com>
2025-02-25 02:09:09 -0800

    comments

--- products/src/main/java/com/products/products/controller/ProductController.java
+++ products/src/main/java/com/products/products/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+//Product Controller class
 @RestController
 @RequestMapping("/api")
 public class ProductController {
