7b7cbe1bedf21bfb2bfd5d829f10d95230453997
unknown <venkateshsr20@gmail.com>
2025-02-27 20:29:57 -0800

    Update functionality working

--- products/src/main/java/com/products/products/controller/ProductController.java
+++ products/src/main/java/com/products/products/controller/ProductController.java
@@ -3,6 +3,13 @@ package com.products.products.controller;
 import com.products.ProductService;
 import com.products.products.OrderRepository;
 import com.products.products.repositories.Orders;
+import io.swagger.v3.oas.annotations.Operation;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +18,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+//Browser URL check - http://localhost:8080/swagger-ui/index.html#/Product/updateProduct
+
 //Product Controller class
+@Tag(name = "Product", description = "Product Management APIs")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class ProductController {
@@ -26,24 +37,26 @@ public class ProductController {
     public String sayHi() {
         return "Hi";
     }
-
-
 //    public List<Orders> getAllOrders(){
 //        return orderRepository.findAll();
 //    }
 
+    @Operation(summary = "Create a new product", tags = { "products", "post" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = {
+                    @Content(schema = @Schema(implementation = Orders.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
     @PostMapping("/Insert")
     private ResponseEntity<Orders> createProduct(@RequestBody Orders orders) {
         Orders savedorder = productService.saveOrder(orders);
         return new ResponseEntity<>(savedorder, HttpStatus.CREATED);
-
     }
 
-//    @GetMapping("/View")
-//    private ResponseEntity<Orders> getAllProducts(){
-//        //return ResponseEntity.;
-//    }
-
+    @Operation(summary = "Update product", tags = { "products", "put" })
+    @ApiResponses({
+       @ApiResponse(responseCode = "200", content = {
+               @Content(schema = @Schema(implementation = Orders.class), mediaType = "application/json") }),
+       @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
     @PutMapping("/Update/{id}")
     private ResponseEntity<Orders> updateProduct(@PathVariable Long id,@RequestBody Orders orders){
         orders =  productService.updateProduct(id, orders);
